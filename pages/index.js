cfb481b19fd617f1c183f5fe60670a9d2ad4e76c
erickvtrr <143184235+erickvtrr@users.noreply.github.com>
2025-06-27 01:37:47 +0000

    alterações

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home () {
-    return <h1> chupa pau, romulo chupa pika, riquelme do cuzin apertado </h1>  
+function Home() {
+  return <h1> chupa pau, romulo chupa pika, riquelme do cuzin apertado </h1>;
+}
+
+function teste() {
+  console.log("teste");
 }
 
 export default Home;
